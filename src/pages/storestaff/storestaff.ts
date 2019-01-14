095ef60ff21593f1e00dff1c7b0d735bcdab3167
qikong333 <qikong333@users.noreply.github.com>
2019-01-14 22:24:31 +0800

    删除店员（接口调试出错）

--- src/pages/storestaff/storestaff.ts
+++ src/pages/storestaff/storestaff.ts
@@ -45,10 +45,16 @@ export class StorestaffPage {
   }
 
   async del(index) {
-    console.log(index)
     // this.lists.splice(index,1)
+    console.log(index);
+    
     let res = await this.http.deleteclerk({ shop_clientid: index });
-
+    if (res.info == "ok" && res.object != undefined) {
+      this.datas = this.datas.filter(r => r.client_id !== index)
+      this.http.presentToast('删除成功')
+    }
+    console.log(res);
+    
   }
 
   showPrompt() {
