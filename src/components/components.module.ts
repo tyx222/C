00ea870bbe34fb6c0c31f601e1ea8821026a5d19
yinweihong <1513372833@qq.com>
2019-01-18 16:06:22 +0800

    1、添加订单详情头部组件

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,20 +1,26 @@
 import { CalendarModule } from 'ion2-calendar';
-import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { GuidanceComponent } from './guidance/guidance';
 import { CalendarPageComponent } from './calendar-page/calendar-page';
 import { MapComponent } from './map/map';
 import { TrickComponent } from './trick/trick';
+import { OrdergoodsComponent } from './ordergoods/ordergoods';
+import { IonicModule } from 'ionic-angular';
 
 @NgModule({
-	declarations: [GuidanceComponent,
-    CalendarPageComponent,
-    MapComponent,
-    TrickComponent],
-	imports: [CalendarModule],
-	exports: [GuidanceComponent,
-	CalendarPageComponent,
-    MapComponent,
-    TrickComponent],
-	schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    declarations: [GuidanceComponent,
+        CalendarPageComponent,
+        MapComponent,
+        TrickComponent,
+        OrdergoodsComponent,
+    ],
+    imports: [CalendarModule, IonicModule],
+    exports: [GuidanceComponent,
+        CalendarPageComponent,
+        MapComponent,
+        TrickComponent,
+        OrdergoodsComponent,
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class ComponentsModule {}
+export class ComponentsModule { }
